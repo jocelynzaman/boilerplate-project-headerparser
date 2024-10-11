adc707eb1bb7475599ae41345facd1d5d0d500e2
Jocelyn Zaman <jzaman@asu.edu>
2024-10-11 10:01:17 -0700

    add whoami endpoint

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  var ip = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255));
+  var lang = req.headers['accept-language'];
+  var sw = req.headers['user-agent'];
+  res.json({ipaddress: ip, language: lang, software: sw})
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
